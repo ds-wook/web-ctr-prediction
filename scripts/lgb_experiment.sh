20c5cdd277a9c7adbaa39612acc3a63dd95930f6
ds-wook <leewook94@gmail.com>
2024-06-02 18:28:55 +0900

    Update!

--- scripts/lgb_experiment.sh
+++ scripts/lgb_experiment.sh
@@ -13,3 +13,18 @@ do
         models.results=5fold-ctr-${MODEL_NAME}-${SAMPLING}-seed${seed} \
         output.name=5fold-ctr-${MODEL_NAME}-${SAMPLING}-seed${seed}
 done
+
+SAMPLING=0.4
+
+for seed in 414 602
+do
+    python src/train.py \
+        data.train=train_sample_${SAMPLING}_seed${seed} \
+        models=${MODEL_NAME} \
+        models.results=5fold-ctr-${MODEL_NAME}-${SAMPLING}-seed${seed}
+
+    python src/predict.py \
+        models=${MODEL_NAME} \
+        models.results=5fold-ctr-${MODEL_NAME}-${SAMPLING}-seed${seed} \
+        output.name=5fold-ctr-${MODEL_NAME}-${SAMPLING}-seed${seed}
+done
